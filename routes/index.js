d709063332d11c9abeb16f9edeede207cea8ef45
Jiye Yu <jiye9701@gmail.com>
2020-10-09 12:22:28 -0400

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
+/*Get home page. */
+router.get('/home', function(req, res, next){
+  res.render('index', { title: 'Home'});
+});
+
+/*Get home page. */
+router.get('/products', function(req, res, next){
+  res.render('index', { title: 'Products'});
+});
+
+/*Get services page. */
+router.get('/services', function(req, res, next){
+  res.render('index', { title: 'Services'});
+});
+
+/*Get about page. */
+router.get('/about', function(req, res, next){
+  res.render('index', { title: 'About'});
+});
+
+/*Get contact page. */
+router.get('/contact', function(req, res, next){
+  res.render('index', { title: 'Contact'});
+});
 module.exports = router;
